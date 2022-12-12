f53cf48a3593bc4e7b59e6840bb549b19542a829
ripbog <crhinesmith5@gmail.com>
2022-12-12 16:37:57 -0500

    servcice worker

---
+++ src/components/ContactMe.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    
+    return (
+      <section id="contact">
+          <div className="row section-head">
+            <div className="ten columns">
+              <p className="lead">
+              Feel free to contact me for any work or suggestions below at my Github or LinkedIn account
+              </p>
+            </div>
+          </div>
+
+        </section>
+        );
+  }
+}
---
+++ src/components/Portfolio.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+const handleClick = (item) => {
+  window.open(
+    `${item}`, "_blank", "noopener, noreferrer"
+  )
+}
+export default class Porfolio extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+        <div className="row">
+          <div className="twelve columns collapsed">
+            <h1>Check Out Some of My Work.</h1>
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+              {
+                resumeData.portfolio && resumeData.portfolio.map((item) => {
+                  return (
+                    <div className="columns portfolio-item">
+                      <div className="item-wrap">
+                        <a onClick={() => handleClick(item.projecturl)}
+                        // href={item.projecturl} 
+                         >
+                          <img src={`${item.imgurl}`} className="item-img" />
+                          <div className="overlay">
+                            <div className="portfolio-item-meta">
+                              <h5>{item.name}</h5>
+                              <p>{item.description}</p>
+                            </div>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
