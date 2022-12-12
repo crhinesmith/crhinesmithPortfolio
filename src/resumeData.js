c2f512b647c349db0cb211989ef1d8f906526d4b
ripbog <crhinesmith5@gmail.com>
2022-12-12 14:28:46 -0500

    app built out, resumedata updloaded

---
+++ src/resumeData.js
@@ -0,0 +1,98 @@
+let resumeData = {
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Calvin Rhinesmith",
+  "role": "FullStack Web Developer",
+  "linkedinId": " https://www.linkedin.com/in/calvin-rhinesmith-225a34192/",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies,",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/calvin-rhinesmith-225a34192/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/crhinesmith",
+      "className": "fa fa-github"
+    },
+  ],
+  "aboutme": "I am a recent college graduate from the University of Kansas with a degree in Chemical Engineering. I have recently completed a course on Fullstack web development from Georgia Tech University",
+  "address": "United States",
+  "website": "https://github.com/crhinesmith",
+  "education": [
+    {
+      "UniversityName": "University of Kansas",
+      "specialization": "Chemical Engineering",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2020",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "UniversityName": "Georgia Tech University",
+      "specialization": "Full Stack Coding Bootcamp",
+      "MonthOfPassing": "Dec",
+      "YearOfPassing": "2022",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "ADP",
+      "specialization": "HR Implementation Consultant",
+      "MonthOfLeaving": "Nov",
+      "YearOfLeaving": "2021 - Now",
+      "Achievements": "Some Achievements"
+    },
+
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Java"
+    },
+    {
+      "skillname": "MySQL"
+    },
+    {
+      "skillname": "MongoDB"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "alDente",
+      "imgurl": "images/portfolio/aldente.jpg",
+      "projecturl": "https://crhinesmith.github.io/alDente/"
+    },
+    {
+      "name": "project2",
+      "description": "MeowWolf",
+      "imgurl": "images/portfolio/meowwoof.jpg",
+      "projecturl": "https://github.com/TeryKing/Project-MeowWoof"
+    },
+    {
+      "name": "project3",
+      "description": "PWA-Text-Editor",
+      "imgurl": "images/portfolio/jate.jpg",
+      "projecturl": "https://github.com/crhinesmith/19-Progressive-Web-Applications-PWA-Text-Editor"
+    },
+    {
+      "name": "project4",
+      "description": "Cargo",
+      "imgurl": "images/portfolio/Image_Coming_Soon.png",
+      "projecturl":"https://github.com/crhinesmith/Car-go"
+    }
+  ],
+
+}
+
+export default resumeData
