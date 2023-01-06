bbdc7563ca3d9407dc49103b1245ae63120139eb
ripbog <crhinesmith5@gmail.com>
2023-01-06 17:15:24 -0500

    fixed line break issue, fixed return true error

--- src/resumeData.js
+++ src/resumeData.js
@@ -16,32 +16,40 @@ let resumeData = {
       "className": "fa fa-github"
     },
   ],
-  "aboutme": "I am a recent college graduate from the University of Kansas with a degree in Chemical Engineering. I have recently completed a course on Fullstack web development from Georgia Tech University",
+  "aboutme": "Full Stack Web Developer with a background in Chemical Engineering and life-long dedication to learning. Effective at combining creativity and problem solving to develop user-friendly applications. Known among staff for strong wit and attention to detail no matter the complexity of the project.",
   "address": "United States",
   "website": "https://github.com/crhinesmith",
   "education": [
-    {
-      "UniversityName": "University of Kansas",
-      "specialization": "Chemical Engineering",
-      "MonthOfPassing": "May",
-      "YearOfPassing": "2020",
-      "Achievements": "Some Achievements"
-    },
     {
       "UniversityName": "Georgia Tech University",
       "specialization": "Full Stack Coding Bootcamp",
       "MonthOfPassing": "Dec",
       "YearOfPassing": "2022",
-      "Achievements": "Some Achievements"
+      "Achievements": ""
+    },
+    {
+      "UniversityName": "University of Kansas",
+      "specialization": "Chemical Engineering",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2020",
+      "Achievements": "Awarded KU Opportunity Grant, KU School of Engineering Scholarship, and James S. Combs Engineering Scholarship; University of Kansas Honors Program | Honors Peer Mentor "
     }
+
   ],
   "work": [
     {
       "CompanyName": "ADP",
       "specialization": "HR Implementation Consultant",
       "MonthOfLeaving": "Nov",
-      "YearOfLeaving": "2021 - Now",
-      "Achievements": "Some Achievements"
+      "YearOfLeaving": "2021 - Present",
+      "Achievements": [
+        "Ensure successful product deployments by architecting, configuring, and testing the Lifion HCM/Payroll solution to meet specific client needs",
+        "Lead remote design workshops to familiarize clients with Lifion cloud processes; develop an understanding of current-state processes gather configuration requirements and share Lifion best practices",
+        "Assist the integration consulting team in helping to configure and test integrations between Lifion and third-party/custom solutions",
+        "Work with the client and internal data migration resources to help inform and support the data conversion process from legacy HR systems Share product knowledge with other consultants and contribute to the continuous improvement of Lifion's implementation methodology"
+      ]
+
+
     },
 
   ],
@@ -57,7 +65,7 @@ let resumeData = {
       "skillname": "Reactjs"
     },
     {
-      "skillname": "Javascript"
+      "skillname": "Java"
     },
     {
       "skillname": "MySQL"
@@ -68,29 +76,23 @@ let resumeData = {
   ],
   "portfolio": [
     {
-      "name": "project1",
-      "description": "alDente",
+      "name": "alDente",
+      "description": "Tools: HTML, CSS, TailwindCSS, Moment.js ",
       "imgurl": "images/portfolio/aldente.jpg",
       "projecturl": "https://crhinesmith.github.io/alDente/"
     },
     {
-      "name": "project2",
-      "description": "MeowWolf",
+      "name": "MeowWoof",
+      "description": "Tools: Node.js, JQuery UI, Handlebars, Bootstrap, Sendgrid, Mysql, and Sequelize, Moment.js ",
       "imgurl": "images/portfolio/meowwoof.jpg",
       "projecturl": "https://github.com/TeryKing/Project-MeowWoof"
     },
     {
-      "name": "project3",
-      "description": "PWA-Text-Editor",
+      "name": "PWA-Text-Editor",
+      "description": "Tools: Webpack, Service Worker API, Express.js ",
       "imgurl": "images/portfolio/jate.jpg",
       "projecturl": "https://github.com/crhinesmith/19-Progressive-Web-Applications-PWA-Text-Editor"
     },
-    {
-      "name": "project4",
-      "description": "Cargo",
-      "imgurl": "images/portfolio/Image_Coming_Soon.png",
-      "projecturl":"https://github.com/crhinesmith/Car-go"
-    }
   ],
 
 }
