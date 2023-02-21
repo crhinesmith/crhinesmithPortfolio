8a249c91eb586547832d8ce6c38f1b0f747ac22b
ripbog <crhinesmith5@gmail.com>
2023-02-21 18:12:39 -0500

    added email

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -7,7 +7,7 @@ export default class ContactUs extends Component {
           <div className="row section-head">
             <div className="ten columns">
               <p className="lead">
-              Feel free to contact me for any work or suggestions below at my Github or LinkedIn account
+              Feel free to contact me for any work or suggestions below at crhinesmith5@gmail.com or via my Github or LinkedIn account
               </p>
             </div>
           </div>
